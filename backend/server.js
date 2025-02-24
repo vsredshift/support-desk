68b28b108866c181310312dea3f962ee644a6272
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-24 12:57:46 +0100

    create basic server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("Server running")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(201).json({message: "Hello"});
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
