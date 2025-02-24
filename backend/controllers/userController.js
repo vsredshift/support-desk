534c5769bea3aacd4573af2148e2113ee8228e1d
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-24 13:22:32 +0100

    add error and exception handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,30 @@
-const registerUser = (req, res) => {
-  res.status(201).send("Register user");
-};
+const asyncHandler = require("express-async-handler");
 
-const loginUser = (req, res) => {
+/**
+ * @description Register a new user
+ * @access      Public
+ * @route       api/users
+ */
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please include all fields");
+  }
+
+  res.status(201).send("Register user");
+});
+
+/**
+ * @description Login a user
+ * @access      Public
+ * @route       api/users/login
+ */
+const loginUser = asyncHandler(async (req, res) => {
   res.status(200).send("Login user");
-};
+});
 
 module.exports = {
   registerUser,
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
-  res.status(201).json({message: "Hello"});
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
 });
 
-app.use("/api/users", require("./routes/userRoutes"))
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
