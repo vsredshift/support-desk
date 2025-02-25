b46d174cf59e2e076e550510b7371964f0eb023e
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-25 14:02:35 +0100

    enable user logout

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -13,8 +13,11 @@ const register = async (userData) => {
   return response.data;
 };
 
+const logout = () => localStorage.removeItem("user");
+
 const authService = {
   register,
+  logout,
 }
 
 export default authService;
