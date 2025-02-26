c79a8028ab4f842e017395dbabe2c7398428ba4d
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-26 13:40:33 +0100

    add tickets to redux

---
+++ frontend/src/features/tickets/ticketSlice.js
@@ -0,0 +1,22 @@
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+
+const initialState = {
+    tickets: [],
+    ticket: {},
+    isError: false,
+    isSuccess: false,
+    isLoading: false,
+    message: "",
+}
+
+export const ticketSlice = createSlice({
+    name: "ticket",
+    initialState,
+    reducers: {
+        reset: (state) => initialState
+    },
+    extraReducers: (builder) => {}
+})
+
+export const {reset} = ticketSlice.actions;
+export default ticketSlice.reducer;
