7f9eb7dfb10b428de6ddbf1c808c379eda7e7790
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-26 14:16:25 +0100

    add create ticket functionality

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/users": {
+      "/api/": {
         target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
