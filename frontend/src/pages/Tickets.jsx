d279755ed7749ab60c9f719def1fea45afca342b
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-26 14:41:20 +0100

    display tickets in client

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -3,6 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getTickets, reset } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
+import TicketItem from "../components/TicketItem";
 
 const Tickets = () => {
   const { tickets, isError, isSuccess, isLoading, message } = useSelector(
@@ -26,8 +27,24 @@ const Tickets = () => {
   if (isLoading) {
     return <Spinner />
   }
-  
-  return <div>Tickets</div>;
+
+  return (
+    <>
+        <BackButton url="/"/>
+        <h1>Tickets</h1>
+        <div className="tickets">
+            <div className="ticket-headings">
+                <div>Date</div>
+                <div>Product</div>
+                <div>Status</div>
+                <div></div>
+            </div>
+            {tickets.map((ticket) => (
+                <TicketItem key={ticket._id} ticket={ticket}/>
+            ))}
+        </div>
+    </>
+  )
 };
 
 export default Tickets;
