6d4bd9396d111ef791327a00c6cfa3f69279490a
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-27 13:22:03 +0100

    remove unnecessary nested routes and hooks

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,15 +1,14 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuthStatus } from "../hooks/useAuthStatus";
-import Spinner from "../components/Spinner";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
-const PrivateRoute = () => {
-  const { loggedIn, checkingStatus } = useAuthStatus();
+const PrivateRoute = ({ children }) => {
+  const { user } = useSelector((state) => state.auth);
 
-  if (checkingStatus) {
-    return <Spinner />;
+  if (user) {
+    return children;
   }
 
-  return loggedIn ? <Outlet /> : <Navigate to="/login" />;
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
