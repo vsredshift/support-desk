0421e1dcf9f24630cf2c16ed87d17cabfce63bd7
Martin O'Shaughnessy <vsredshift@gmail.com>
2025-02-27 15:52:45 +0100

    add sorting on tickets page

--- frontend/src/pages/Tickets.jsx
+++ frontend/src/pages/Tickets.jsx
@@ -1,14 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getTickets} from "../features/tickets/ticketSlice";
+import { getTickets } from "../features/tickets/ticketSlice";
 import Spinner from "../components/Spinner";
 import BackButton from "../components/BackButton";
 import TicketItem from "../components/TicketItem";
 
 const Tickets = () => {
-  const { tickets } = useSelector(
-    (state) => state.tickets
-  );
+  const [sortOrder, setSortOrder] = useState("desc");
+  const { tickets } = useSelector((state) => state.tickets);
 
   const dispatch = useDispatch();
 
@@ -16,27 +15,40 @@ const Tickets = () => {
     dispatch(getTickets());
   }, [dispatch]);
 
+  const toggleSortOrder = () =>
+    setSortOrder(sortOrder === "desc" ? "asc" : "desc");
+
   if (!tickets) {
-    return <Spinner />
+    return <Spinner />;
   }
 
   return (
     <>
-        <BackButton url="/"/>
-        <h1>Tickets</h1>
-        <div className="tickets">
-            <div className="ticket-headings">
-                <div>Date</div>
-                <div>Product</div>
-                <div>Status</div>
-                <div></div>
-            </div>
-            {tickets && tickets.length > 0 ? (tickets.map((ticket) => (
-                <TicketItem key={ticket._id} ticket={ticket}/>
-            ))) : <p>No tickets to show</p>}
+      <BackButton url="/" />
+      <h1>Tickets</h1>
+      <div className="tickets">
+        <div className="ticket-headings">
+          <div>Date</div>
+          <div>Product</div>
+          <div>Status</div>
+          <div className="btn btn-ticket-order" onClick={toggleSortOrder}>
+            Show {sortOrder === "asc" ? "newest" : "oldest"} first
+          </div>
         </div>
+        {tickets && tickets.length > 0 ? (
+          [...tickets]
+            .sort((a, b) =>
+              sortOrder === "desc"
+                ? new Date(b.createdAt) - new Date(a.createdAt)
+                : new Date(a.createdAt) - new Date(b.createdAt)
+            )
+            .map((ticket) => <TicketItem key={ticket._id} ticket={ticket} />)
+        ) : (
+          <p>No tickets to show</p>
+        )}
+      </div>
     </>
-  )
+  );
 };
 
 export default Tickets;
